981c2ab8bdcb7eaa4d729d920c990c61ba386536
saki <saki@saki.saki>
2017-03-21 12:45:08 +0800

    修改复杂流程merge时无限循环问题

--- snaker-core/src/main/java/org/snaker/engine/handlers/impl/MergeBranchHandler.java
+++ snaker-core/src/main/java/org/snaker/engine/handlers/impl/MergeBranchHandler.java
@@ -14,49 +14,56 @@
  */
 package org.snaker.engine.handlers.impl;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import org.snaker.engine.model.ForkModel;
-import org.snaker.engine.model.JoinModel;
-import org.snaker.engine.model.NodeModel;
-import org.snaker.engine.model.TransitionModel;
-import org.snaker.engine.model.WorkModel;
+import org.snaker.engine.model.*;
 
 /**
  * 合并分支操作的处理器
+ * 
  * @author yuqs
  * @since 1.0
  */
 public class MergeBranchHandler extends AbstractMergeHandler {
 	private JoinModel model;
+
 	public MergeBranchHandler(JoinModel model) {
 		this.model = model;
 	}
-	
+
 	/**
 	 * 对join节点的所有输入变迁进行递归，查找join至fork节点的所有中间task元素
+	 * 
 	 * @param node
-	 * @param buffer
+	 * @param taskNames
 	 */
-	private void findForkTaskNames(NodeModel node, StringBuilder buffer) {
-		if(node instanceof ForkModel) return;
+	private void findForkTaskNames(NodeModel node, Set<String> taskNames) {
+		if (node instanceof ForkModel) {
+			return;
+		}
 		List<TransitionModel> inputs = node.getInputs();
-		for(TransitionModel tm : inputs) {
-			if(tm.getSource() instanceof WorkModel) {
-				buffer.append(tm.getSource().getName()).append(",");
+		for (TransitionModel tm : inputs) {
+			NodeModel source = tm.getSource();
+			String name = source.getName();
+			if (!taskNames.contains(name)) {
+				if (source instanceof WorkModel) {
+					taskNames.add(name);
+				}
+				findForkTaskNames(source, taskNames);
 			}
-			findForkTaskNames(tm.getSource(), buffer);
 		}
 	}
 
 	/**
 	 * 对join节点的所有输入变迁进行递归，查找join至fork节点的所有中间task元素
+	 * 
 	 * @see org.snaker.engine.handlers.impl.AbstractMergeHandler#findActiveNodes()
 	 */
 	protected String[] findActiveNodes() {
-		StringBuilder buffer = new StringBuilder(20);
-		findForkTaskNames(model, buffer);
-		String[] taskNames = buffer.toString().split(",");
-		return taskNames;
+		Set<String> taskNames = new HashSet<String>();
+		findForkTaskNames(model, taskNames);
+		return taskNames.toArray(new String[] {});
 	}
 }
